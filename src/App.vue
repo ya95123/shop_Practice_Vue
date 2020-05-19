8e120b266bd39c5d282d8d33b557fc31b7abff71
ya95123 <ya95123@gmail.com>
2020-05-19 10:44:49 +0800

    版權宣告

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
     Marquee1
     Content
     Footer
+    //- 版權宣告
+    <div>Icons made by <a href="http://www.dariusdan.com/" title="Darius Dan">Darius Dan</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 </template>
 
 <script>
