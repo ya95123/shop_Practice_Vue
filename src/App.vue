8a08035a608671508acbe8f4118b095bd276a9b6
ya95123 <ya95123@gmail.com>
2020-05-19 09:04:54 +0800

    Marquee1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template lang="pug">
   #app
     Top
+    Marquee1
 </template>
 
 <script>
 import Top from './components/Top'
+import Marquee1 from './components/Marquee1'
 
 export default {
   name: 'App',
   components: {
-    Top
+    Top,
+    Marquee1
   }
 }
 </script>
---
+++ src/components/Marquee1.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  #Marquee1
+    marquee 大家來購物網！精品商品特價大拍賣！要買要快！
+</template>
+
+<script>
+export default {
+
+}
+</script>
