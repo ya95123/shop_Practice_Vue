45a26fe76b24db022339f206f307a17e9418c601
ya95123 <ya95123@gmail.com>
2020-05-19 09:25:19 +0800

    content

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   #app
     Top
     Marquee1
+    Content
 </template>
 
 <script>
 import Top from './components/Top'
 import Marquee1 from './components/Marquee1'
+import Content from './components/Content'
 
 export default {
   name: 'App',
   components: {
     Top,
-    Marquee1
+    Marquee1,
+    Content
   }
 }
 </script>
---
+++ src/components/Content.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  #Content
+    table(border="1" cellspacing="0")
+      tr(v-for="(cat,index) in categories" :key="index")
+        td {{cat.name}}
+        td
+          img(:src="cat.item1")
+        td
+          img(:src="cat.item2")
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      categories: [
+        {
+          name: '流行皮件',
+          item1: './img/0403.jpg',
+          item2: './img/0404.jpg'
+        },
+        {
+          name: '流行鞋區',
+          item1: './img/0405.jpg',
+          item2: './img/0406.jpg'
+        },
+        {
+          name: '流行精品',
+          item1: './img/0407.jpg',
+          item2: './img/0408.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
