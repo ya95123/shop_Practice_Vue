869259ac39fefb0fc8bc2e437a442df15f903d36
ya95123 <ya95123@gmail.com>
2020-05-19 08:52:12 +0800

    top

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template lang="pug">
-
+  #app
+    Top
 </template>
 
 <script>
+import Top from './components/Top'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Top
+  }
 }
 </script>
--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
-
+  #Top
+    | 流行精品購物網站
 </template>
 
 <script>
