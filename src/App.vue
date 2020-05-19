446885fac89c2a38bd9eddaaba36c090a5d56223
ya95123 <ya95123@gmail.com>
2020-05-19 09:37:27 +0800

    Footer

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,22 @@
     Top
     Marquee1
     Content
+    Footer
 </template>
 
 <script>
 import Top from './components/Top'
 import Marquee1 from './components/Marquee1'
 import Content from './components/Content'
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
   components: {
     Top,
     Marquee1,
-    Content
+    Content,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  #Footer
+    span
+      a(href="#") 訪客留言版
+    br
+    span 網頁設計及維護：XX○○○、最近更新日期：yyyy/mm/dd
+</template>
+
+<script>
+export default {
+
+}
+</script>
